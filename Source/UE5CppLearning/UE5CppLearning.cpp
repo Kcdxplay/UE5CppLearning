942e8ee5a16c3ae9c37c02e1b5c0fa09c44b28db
Kcdxplay <kcdxplay@gmail.com>
2022-12-12 11:01:34 +0800

    Inital Project

---
+++ Source/UE5CppLearning/UE5CppLearning.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5CppLearning.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5CppLearning, "UE5CppLearning" );
---
+++ Source/UE5CppLearning/UE5CppLearningGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5CppLearningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5CPPLEARNING_API AUE5CppLearningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
